57fc41502f25c6edfe5f863ed7aa6b80888b169e
jimohsegun1 <jimohsegunj@gmail.com>
2025-04-15 16:27:30 +0100

    update controller

--- backend/src/controller/book.controller.js
+++ backend/src/controller/book.controller.js
@@ -1,110 +1,115 @@
 import Book from "../models/book.model.js";
 import cloudinary from "../lib/cloudinary.js";
+import multer from "multer";
+
+ // use multer for image storage
+ const upload = multer();
 
 export const getAllBooks = async (req, res) => {
-    try {
-        //example call from react native - frontend
-        // const response = await fetch("http://localhost:3000/api/books?page=1&limit=5");
-        const page = req.query.page || 1;
-        const limit = req.query.limit || 5;
-        const skip = (page - 1) * limit;
-        const totalBooks = await Book.countDocuments();
-        const totalPages = Math.ceil(totalBooks / limit);
+  try {
+    //example call from react native - frontend
+    // const response = await fetch("http://localhost:3000/api/books?page=1&limit=5");
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 5;
+    const skip = (page - 1) * limit;
+    const totalBooks = await Book.countDocuments();
+    const totalPages = Math.ceil(totalBooks / limit);
 
-        const books = await Book.find()
-            .sort({ createdAt: -1 })
-            .skip(skip)
-            .limit(limit)
-            .populate("user", "username profileImage")
-            .exec();
+    const books = await Book.find()
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .populate("user", "username profileImage")
+      .exec();
 
-        res.status(200).json({
-            books,
-            currentPage: page,
-            totalBooks,
-            totalPages,
-        });
-    } catch (error) {
-        console.log("Error in getAllBooks controller", error.message);
-        res.status(500).json({ message: error.message });
-    }
+    res.status(200).json({
+      books,
+      currentPage: page,
+      totalBooks,
+      totalPages,
+    });
+  } catch (error) {
+    console.log("Error in getAllBooks controller", error.message);
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const createBook = async (req, res) => {
-    try {
-        const { title, caption, rating, image } = req.body;
+ 
+  try {
+    const { title, caption, rating, image } = req.body;
 
-        if (!title || !caption || !rating || !image) {
-            return res.status(400).json({ message: "All fields are required" });
-        }
-
-        const uploadResponse = await cloudinary.uploader.upload(image);
-        const imageUrl = uploadResponse.secure_url;
-
-        const newBook = await Book.create({
-            title,
-            caption,
-            rating,
-            image: imageUrl,
-            user: req.user._id,
-        });
-
-        await newBook.save();
-
-        res.status(201).json(newBook);
-    } catch (error) {
-        console.log("Error in createBook controller", error);
-        res.status(500).json({ message: error.message });
+    if (!title || !caption || !rating || !image) {
+      return res.status(400).json({ message: "All fields are required" });
     }
+
+    const uploadResponse = await cloudinary.uploader.upload(image);
+    const imageUrl = uploadResponse.secure_url;
+
+    const newBook = await Book.create({
+      title,
+      caption,
+      rating,
+      image: imageUrl,
+      user: req.user._id,
+    });
+
+    await newBook.save();
+
+    res.status(201).json(newBook);
+  } catch (error) {
+    console.log("Error in createBook controller", error);
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const deleteBook = async (req, res) => {
-    try {
-        const bookId = req.params.id;
+  try {
+    const bookId = req.params.id;
 
-        const book = await Book.findById(bookId);
+    const book = await Book.findById(bookId);
 
-        if (!book) {
-            return res.status(404).json({ message: "Book not found" });
-        }
-
-        if (book.user.toString() !== req.user._id.toString()) {
-            return res.status(401).json({ message: "Unauthorized" });
-        }
-
-        //delete image from cloudinary
-        if (book.image && book.image.includes("cloudinary")) {
-            const publicId = book.image.split("/").pop().split(".")[0];
-            try {
-                await cloudinary.uploader.destroy(`book/${publicId}`);
-            } catch (error) {
-                console.log("Error deleting image from cloudinary", error.message);
-            }
-        }
-
-        await Book.findByIdAndDelete(bookId);
-
-        res.status(200).json({ message: "Book deleted successfully" });
-    } catch (error) {
-        console.log("Error in deleteBook controller", error.message);
-        res.status(500).json({ message: error.message });
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
     }
+
+    if (book.user.toString() !== req.user._id.toString()) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    //delete image from cloudinary
+    if (book.image && book.image.includes("cloudinary")) {
+      const publicId = book.image.split("/").pop().split(".")[0];
+      try {
+        await cloudinary.uploader.destroy(`book/${publicId}`);
+      } catch (error) {
+        console.log("Error deleting image from cloudinary", error.message);
+      }
+    }
+
+    await Book.findByIdAndDelete(bookId);
+
+    res.status(200).json({ message: "Book deleted successfully" });
+  } catch (error) {
+    console.log("Error in deleteBook controller", error.message);
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const recommendedBooks = async (req, res) => {
-    try {
-        const userId = req.user._id;
+  try {
+    const userId = req.user._id;
 
-        // my recommended book
-        const books = await Book.find({ user: userId })
-            .sort({ createdAt: -1 })
-            .limit(5)
-            .populate("user", "username profileImage")
-            .exec();
+    // my recommended book
+    const books = await Book.find({ user: userId })
+      .sort({ createdAt: -1 })
+      .limit(5)
+      .populate("user", "username profileImage")
+      .exec();
 
-        res.status(200).json(books);
-    } catch (error) {
-        console.log("Error in recommendedBooks controller", error.message);
-        res.status(500).json({ message: error.message });
-    }
+    res.status(200).json(books);
+  } catch (error) {
+    console.log("Error in recommendedBooks controller", error.message);
+    res.status(500).json({ message: error.message });
+  }
 };
--- mobile/store/bookStore.js
+++ mobile/store/bookStore.js
@@ -6,25 +6,54 @@ export const useBookStore = create((set) => ({
     isLoading: false,
     error: null,
 
+    // addBook: async (bookData) => {
+    //     try {
+    //         const token = await AsyncStorage.getItem("token");
+    //         const response = await fetch(`${API_URL}/books`, {
+    //             method: "POST",
+    //             headers: {
+    //                 "Content-Type": "application/json",
+    //                 Authorization: `Bearer ${token}`,
+    //             },
+    //             body: JSON.stringify(bookData),
+    //         });
+    //         const data = await response.json();
+    //         if (response.ok) {
+    //             return { success: true };
+    //         } else {
+    //             throw new Error(data.message);
+    //         }
+    //     } catch (error) {
+    //         return { success: false, error: error.message };
+    //     }
+    // },
+
     addBook: async (bookData) => {
+        console.log('Adding book with data:', bookData);
         try {
-            const token = await AsyncStorage.getItem("token");
-            const response = await fetch(`${API_URL}/books`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`,
-                },
-                body: JSON.stringify(bookData),
-            });
-            const data = await response.json();
-            if (response.ok) {
-                return { success: true };
-            } else {
-                throw new Error(data.message);
-            }
+          const token = await AsyncStorage.getItem("token");
+          console.log('Token:', token);
+          const response = await fetch(`${API_URL}/books`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify(bookData),
+          });
+          console.log('Response:', response);
+          const data = await response.json();
+          console.log('Data:', data);
+          if (response.ok) {
+            console.log('Book added successfully!');
+            return { success: true };
+          } else {
+            console.log('Error adding book:', data.message);
+            throw new Error(data.message);
+          }
         } catch (error) {
-            return { success: false, error: error.message };
+          console.log('Error adding book:', error.message);
+          return { success: false, error: error.message };
         }
-    },
+      }
 }));
