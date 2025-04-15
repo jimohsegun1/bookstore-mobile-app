cad7bac6c6a9254b7db9de7918a7c060c2ae5f33
jimohsegun1 <jimohsegunj@gmail.com>
2025-04-15 16:58:53 +0100

    routes updated

--- backend/src/routes/book.routes.js
+++ backend/src/routes/book.routes.js
@@ -1,11 +1,22 @@
 import express from "express";
-import { createBook, deleteBook, getAllBooks, recommendedBooks } from "../controller/book.controller.js";
+import {
+  createBook,
+  deleteBook,
+  getAllBooks,
+  recommendedBooks,
+} from "../controller/book.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import multer from "multer";
+
+// use multer for image storage
+const upload = multer({ dest: "uploads/" });
 
 const router = express.Router();
 
 router.get("/", protectRoute, getAllBooks);
-router.post("/", protectRoute, createBook);
+// router.post("/", protectRoute, createBook);
+router.post("/", protectRoute, upload.single("image"), createBook);
+
 router.delete("/:id", protectRoute, deleteBook);
 router.get("/user", protectRoute, recommendedBooks);
 
